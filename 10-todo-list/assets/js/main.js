b9200089456f6838d158a41320d219bb562ac4a5
NguyenVanTu2903 <nguyenvantuha123@gmail.com>
2024-04-30 16:17:47 +0700

    Update

--- 10-todo-list/assets/js/main.js
+++ 10-todo-list/assets/js/main.js
@@ -1,3 +1,44 @@
 const inputTodo = document.querySelector("#input");
 const btnAdd = document.querySelector(".btn");
-const listTodo = document.querySelectorAll(".list");
+const listTodo = document.querySelector(".list");
+
+let todoList;
+let inputValue = "";
+
+function save() {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function render() {
+    todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+    const html = todoList
+        .map(
+            (todo) =>
+                `<li class="item">${todo} <button class="btnDelete">&times;</button></li>`
+        )
+        .join("");
+
+    listTodo.innerHTML = html;
+
+    const btnDelete = document.querySelectorAll(".btnDelete");
+    btnDelete.forEach((btn, idx) => {
+        btn.addEventListener("click", (e) => {
+            todoList.splice(idx, 1);
+            e.target.parentElement.remove();
+            save();
+            render();
+        });
+    });
+}
+inputTodo.addEventListener("input", function (e) {
+    inputValue = this.value;
+});
+
+btnAdd.addEventListener("click", function (e) {
+    e.preventDefault();
+    todoList.push(inputValue);
+    save();
+    render();
+    inputTodo.value = "";
+});
+render();
